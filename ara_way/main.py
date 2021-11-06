5864c3938e85dbc6ecc3a635f502fe324323cdd9
Anton <over.uran@gmail.com>
2021-11-06 11:16:59 +0100

    echo bot

--- ara_way/main.py
+++ ara_way/main.py
@@ -1,3 +1,15 @@
 """ARA IN THE WAY!"""
 
 import telebot
+import config
+
+bot = telebot.TeleBot(config.TOKEN)
+
+
+@bot.message_handler(content_types=['text'])
+def echo(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
